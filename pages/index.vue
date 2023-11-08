1a7af64779c7558c8b5e9c739191437c0e1c1c1c
Eleonora <ezhyrlitsyna@gmail.com>
2023-11-08 09:13:54 +0200

    add toggle theme btn

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,13 @@
 <template>
+  <button
+    @click="theme()"
+    class="mt-4 w-12 h-6 rounded-full p-1 bg-gray-400 dark:bg-gray-600 relative transition-colors duration-500 ease-in focus:outline-none focus:ring-2 focus:ring-blue-700 dark:focus:ring-blue-600 focus:border-transparent"
+  >
+    <div
+      id="toggle"
+      class="rounded-full w-4 h-4 bg-blue-600 dark:bg-blue-500 relative ml-0 dark:ml-6 pointer-events-none transition-all duration-300 ease-out"
+    />
+  </button>
   <div class="flex h-screen w-full justify-center items-center">
     <Dropdown
       :items="items"
@@ -51,4 +60,23 @@ const items = [
     },
   ],
 ];
+
+function theme() {
+  document.documentElement.classList.toggle('dark');
+}
 </script>
+<style>
+html.dark {
+  --tw-bg-opacity: 1;
+  background-color: rgba(31, 41, 55 var(--tw-bg-opacity));
+}
+
+html.dark .dark\:ml-6 {
+  margin-left: 1.5rem;
+}
+
+html.dark .dark\:bg-blue-500 {
+  --tw-bg-opacity: 1;
+  background-color: rgba(59, 130, 246, var(--tw-bg-opacity));
+}
+</style>
